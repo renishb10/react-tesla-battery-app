6a5b8ba065d8a001361aff924a7470cf2cc8dd13
Renish B <renish.bhaskaran@globalenglish.com>
2019-03-17 16:44:35 +0530

    added wheel component

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './TeslaBattery.css';
+
+//Import components here
+import TeslaNotice from './TeslaNotice/TeslaNotice';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
+
 class TeslaBattery extends React.Component {
   render() {
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
+        <TeslaCar wheelsize={19}/>
+        <TeslaNotice />
       </form>
     )
   }
